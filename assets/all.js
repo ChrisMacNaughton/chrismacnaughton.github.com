b204c28edc60358085e1fe20f3ac9271c5ac5810
Chris MacNaughton <chmacnaughton@gmail.com>
2013-07-05 22:14:42 -0400

    have some basic search working

--- assets/all.js
+++ assets/all.js
@@ -3,4 +3,5 @@
 {% include assets/js/jquery.scrolldepth.min.js %}
 {% include assets/js/bootstrap.min.js %}
 {% include assets/js/shadowbox.js %}
-{% include assets/js/lunr.min.js %}
+{% include assets/js/date.format.js %}
+{% include assets/js/uri.min.js %}
